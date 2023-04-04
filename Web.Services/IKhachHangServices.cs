fd976bcc9d1ba53191ad4b788742229aeb154b5e
Quang Long <trinhq011@gmail.com>
2023-04-04 22:51:39 +0700

    Khách Hàng chưa xong đợi Bùi làm

---
+++ Web.Services/IKhachHangServices.cs
@@ -0,0 +1,15 @@
+﻿using Web.Entities;
+
+namespace Web.Services
+{
+    public interface IKhachHangServices
+    {
+        IEnumerable<KhachHang> GetAll();
+        KhachHang GetById(int id);
+        Task CreateNhanVienAsSync(KhachHang khachHang);
+        Task UpdateNhanVienAsSyncs(KhachHang khachHang);
+        Task UpdateById(int id);
+        Task DeleteById(int id);
+        Task DeleteNhanVien(KhachHang khachHang);
+    }
+}
---
+++ Web.Services/implementation/KhachHangServices.cs
@@ -0,0 +1,50 @@
+﻿using Web.Entities;
+using Web.Persistances;
+
+namespace Web.Services.implementation
+{
+    public class KhachHangServices : IKhachHangServices
+    {
+        private ApplicationDbContext _context;
+
+        public KhachHangServices(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateNhanVienAsSync(KhachHang khachHang)
+        {
+            _context.Add(khachHang);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteNhanVien(KhachHang khachHang)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<KhachHang> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public KhachHang GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateNhanVienAsSyncs(KhachHang khachHang)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
