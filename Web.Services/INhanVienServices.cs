0398bcd9dadfd7a2fa79c571bb2634eb30c62d72
Quang Long <trinhq011@gmail.com>
2023-04-04 22:40:26 +0700

    Giao diện và Services

---
+++ Web.Services/INhanVienServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Entities;
+
+namespace Web.Services
+{
+    public interface INhanVienServices
+    {
+        IEnumerable<NhanVien> GetAll();
+        NhanVien GetById(int id);
+        Task CreateNhanVienAsSync(NhanVien nhanVien);
+        Task UpdateNhanVienAsSyncs(NhanVien nhanVien);
+        Task UpdateById(int id);
+        Task DeleteById(int id);
+        Task DeleteNhanVien(NhanVien nhanVien);
+    }
+}
---
+++ Web.Services/implementation/NhanVienServices.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Entities;
+using Web.Persistances;
+
+namespace Web.Services.implementation
+{
+    public class NhanVienServices : INhanVienServices
+    {
+        private ApplicationDbContext _context;
+
+        public NhanVienServices(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateNhanVienAsSync(NhanVien nhanVien)
+        {
+            _context.Add(nhanVien);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteById(int id)
+        {
+            var nhanVien = GetById(id);
+            if (nhanVien != null)
+            {
+                _context.NhanVien.Remove(nhanVien);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteNhanVien(NhanVien nhanVien)
+        {
+            _context?.NhanVien.Remove(nhanVien);
+            await _context.SaveChangesAsync();
+        }
+
+        public IEnumerable<NhanVien> GetAll()
+        {
+            return _context.NhanVien.ToList();
+        }
+
+        public NhanVien GetById(int id)
+        {
+            return _context.NhanVien.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public async Task UpdateById(int id)
+        {
+            var nhanVien = GetById(id);
+            _context.NhanVien.Update(nhanVien);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateNhanVienAsSyncs(NhanVien nhanVien)
+        {
+            _context.NhanVien.Update(nhanVien);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
