f835f43d764e12a995338c86cc56cfa0369b2048
Quang Long <trinhq011@gmail.com>
2023-04-22 21:14:06 +0700

    Title View

---
+++ Web-BanDienThoai/Controllers/DashBoardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_BanDienThoai.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
