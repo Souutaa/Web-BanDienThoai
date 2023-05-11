3f30c22d78fae99e74adca9ed2bd7a345a6b30ec
Shikigami Maito <shikigamimaito25@gmail.com>
2023-05-11 07:23:32 +0700

    fix cart

---
+++ Web-BanDienThoai/Models/Cart/CartProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Web_BanDienThoai.Models.Cart
+{
+    public class CartProductViewModel
+    {
+        public string Id_SanPham { get; set; }
+
+        public long SoLuongSanPham { get; set; }
+
+        public double SubTotal { get; set; }
+        // gia san pham * so luong
+    }
+}
